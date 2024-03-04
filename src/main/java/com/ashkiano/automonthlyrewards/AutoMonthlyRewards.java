c59cace318bf66518c38d77f27d8ffae53c93aca
Josef Vyskoèil <josef.vyskocil@cleverlance.com>
2024-03-04 23:27:02 +0100

    init

---
+++ src/main/java/com/ashkiano/automonthlyrewards/AutoMonthlyRewards.java
@@ -0,0 +1,41 @@
+package com.ashkiano.automonthlyrewards;
+
+import org.bukkit.event.Listener;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import java.util.Calendar;
+
+public class AutoMonthlyRewards extends JavaPlugin implements Listener {
+    @Override
+    public void onEnable() {
+        saveDefaultConfig();
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        new BukkitRunnable() {
+            public void run() {
+                String playerName = event.getPlayer().getName();
+                Calendar now = Calendar.getInstance();
+                int currentMonth = now.get(Calendar.MONTH);
+                int currentYear = now.get(Calendar.YEAR);
+                int lastClaimedMonth = getConfig().getInt("rewards." + playerName + ".month", -1);
+                int lastClaimedYear = getConfig().getInt("rewards." + playerName + ".year", -1);
+
+                if (currentYear != lastClaimedYear || currentMonth != lastClaimedMonth) {
+                    String rewardCommand = getConfig().getString("reward-command").replace("%player%", playerName);
+                    getServer().dispatchCommand(getServer().getConsoleSender(), rewardCommand);
+                    event.getPlayer().sendMessage("You have claimed your monthly reward.");
+
+                    getConfig().set("rewards." + playerName + ".month", currentMonth);
+                    getConfig().set("rewards." + playerName + ".year", currentYear);
+                    saveConfig();
+                }
+            }
+        }.runTaskLater(this, 1200L);
+    }
+}
